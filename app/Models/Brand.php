028bcc9619bccf5e3ed1587113f6ecf76e8567a2
Labnan <khalidmasum@iut-dhaka.edu>
2020-11-29 19:39:57 +0600

    created model brand

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -5,13 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Brand extends Model
 {
     use HasFactory;
 
-    public function brand(){
-        return $this->belongsTo(Brand::class);
+    public function products(){
+        return $this->hasMany(Product::class);
     }
-
-
 }
