e25f5e171df3ffba2a88d41d0361d5924a4edba0
Labnan <khalidmasum@iut-dhaka.edu>
2020-11-29 15:02:01 +0600

    Altered table Products added name, price, description

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -10,6 +10,8 @@
  */
 
 
+
+
 namespace App\Models{
 /**
  * App\Models\User
--- database/migrations/2020_11_29_084944_create_products_table.php
+++ database/migrations/2020_11_29_084944_create_products_table.php
@@ -16,6 +16,12 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            //Todo: product type
+            $table->unsignedInteger('price');
+            $table->text('description');
+            //Todo: Earned Points
+            //Todo: Brand
         });
     }
 
