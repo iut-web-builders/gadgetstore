0f5acc727cfb010dfd1da485f2dc18ba4949a293
Labnan <khalidmasum@iut-dhaka.edu>
2020-12-04 03:14:56 +0600

    Fixing customerRegistration route

--- routes/web.php
+++ routes/web.php
@@ -29,20 +29,20 @@ Route::resource('/products',ProductsController::class);
 
 Auth::routes();
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+
 
 Route::get('/login/mod',[LoginController::class,'showModLoginForm']);
 Route::get('/login/customer', [LoginController::class,'showCustomerLoginForm']);
 Route::get('/register/mod', [RegisterController::class,'showModRegistrationForm']);
-Route::get('/register/customer', [RegisterController::class,'showCustomerRegisterForm']);
+Route::get('/register/customer', [RegisterController::class,'showCustomerRegistrationForm']);
 
 Route::post('/login/mod', [LoginController::class,'modLogin']);
 Route::post('/login/customer', [LoginController::class,'customerLogin']);
 Route::post('/register/mod', [RegisterController::class,'createMod']);
 Route::post('/register/customer', [RegisterController::class,'createCustomer']);
 
-Route::view('/home', 'home')->middleware('auth');
+//Route::view('/home', 'home')->middleware('auth');
 Route::view('/mod', 'mod');
 Route::view('/customer', 'customer');
